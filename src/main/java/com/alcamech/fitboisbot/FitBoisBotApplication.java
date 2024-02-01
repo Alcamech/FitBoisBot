8dd6c25746b2050dde239f78eec07c26f8238857
Lawton <alcamech@gmail.com>
2024-02-01 09:08:07 -0500

    Enabled Scheduling

--- src/main/java/com/alcamech/fitboisbot/FitBoisBotApplication.java
+++ src/main/java/com/alcamech/fitboisbot/FitBoisBotApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @PropertySources({
         @PropertySource("classpath:application.properties"),
         @PropertySource("classpath:dev.properties")
 })
+@EnableScheduling
 public class FitBoisBotApplication {
 
     public static void main(String[] args) {
