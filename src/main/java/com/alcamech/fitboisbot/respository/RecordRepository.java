b04edb9657694944db868849466352a9c11e5b59
Lawton <alcamech@gmail.com>
2024-02-01 10:07:51 -0500

    FitBoi Rewards c6

--- src/main/java/com/alcamech/fitboisbot/respository/RecordRepository.java
+++ src/main/java/com/alcamech/fitboisbot/respository/RecordRepository.java
@@ -17,10 +17,10 @@ public interface RecordRepository extends CrudRepository<FitBoiRecord, Integer> 
     @Query("SELECT COUNT(*) FROM FitBoiRecord WHERE userId = :userId AND year = YEAR(CURDATE()) AND month = :month")
     Long countByUserIdWithCurrentYearAndMonth(@Param("userId") Long userId, @Param("month") String month);
 
-    @Query(value = "SELECT MAX(activity_count) FROM (SELECT COUNT(*) AS activity_count FROM FitBoiRecord WHERE year = YEAR(CURDATE()) " +
+    @Query(value = "SELECT MAX(activity_count) FROM (SELECT COUNT(*) AS activity_count FROM fit_boi_record WHERE year = YEAR(CURDATE()) " +
             "AND month = :month GROUP BY user_id) AS subquery", nativeQuery = true)
     Long findMaxActivityCountByYearAndMonth(@Param("month") String month);
 
-    @Query(value = "SELECT user_id FROM FitBoiRecord WHERE year = YEAR(CURDATE()) AND month = :month GROUP BY user_id HAVING COUNT(*) = :maxCount", nativeQuery = true)
+    @Query(value = "SELECT user_id FROM fit_boi_record WHERE year = YEAR(CURDATE()) AND month = :month GROUP BY user_id HAVING COUNT(*) = :maxCount", nativeQuery = true)
     List<Long> findAllUsersWithMaxCount(@Param("month") String month, @Param("maxCount") Long maxCount);
 }
